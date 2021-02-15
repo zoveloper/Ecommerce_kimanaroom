306e4ec798cee5690db55794d5bafc253b1c22ca
Martin Cho <martincho@Martins-MacBook-Pro.local>
2021-02-14 23:22:19 -0500

    Added Product Details Screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,11 +16,11 @@ export default function Header() {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
                         <LinkContainer to='/cart'>
-                            <Nav.Link ><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                            <Nav.Link ><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
                         </LinkContainer>
 
                         <LinkContainer to='/login'>
-                            <Nav.Link ><i className="fas fa-user"></i>Login</Nav.Link>
+                            <Nav.Link ><i className="fas fa-user"></i> Login</Nav.Link>
                         </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,67 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-function ProductScreen() {
+function ProductScreen({ match }) {
+    const product = products.find((p) => p._id == match.params.id)
     return (
         <div>
-            Product
+            <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+            <Row>
+                <Col md={6}>
+                        <Image src={product.image} alt={product.name} fluid/>
+                </Col>
+
+                <Col md={3}>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <h3>{product.name}</h3>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                Price: ${product.price}
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                Description: ${product.description}
+                            </ListGroup.Item>
+                        </ListGroup>
+                </Col>
+                    <Col md={3}>    
+                        <Card>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
+                                    <Row>
+                                        <col>proce:</col>
+                                        <col>
+                                            <strong>${product.price}</strong>
+                                        </col>
+                                    </Row>
+                                </ListGroup.Item>
+
+
+                                <ListGroup.Item>
+                                    <Row>
+                                        <col>Status</col>
+                                        <col>
+                                            {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                        </col>
+                                    </Row>
+                                </ListGroup.Item>
+
+                                <ListGroupItem>
+                                    <Button className='btn-block' disabled={product.countInStock == 0}type='button'>ADD TO CART</Button>
+                                </ListGroupItem>
+                            </ListGroup>
+                        </Card>
+                    </Col>
+            </Row>
         </div>
     )
 }
