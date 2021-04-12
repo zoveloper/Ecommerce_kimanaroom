9c2c67792ad68238fcb3025665312606f0875d95
Martin Cho <martincho@Martins-MacBook-Pro.local>
2021-04-11 20:14:40 -0400

    UPDATED DB_2

--- backend/settings.py
+++ backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kimanaroom.herokuapp.com']
 
